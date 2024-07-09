6bef9bccdaa11445064e7fd63a4730a5432a39e0
EaganOG <connelleaganwebb@gmail.com>
2024-07-09 18:07:27 +1200

    created crouch function and changed Grunt mesh to be a different shape

--- Player/Scripts/playerCharacter.gd
+++ Player/Scripts/playerCharacter.gd
@@ -3,6 +3,11 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 5.5
+var is_crouching = false
+var crouch_speed = 2.0
+var original_height = Vector3(1.0,1.0,1.0)
+var crouch_height = Vector3(1.0,0.5,1.0)
+var current_height = original_height
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -11,16 +16,21 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
 
+func _ready():
+	current_height = original_height
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed("exit"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().quit()
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			neck.rotate_y(-event.relative.x * 0.01)
 			camera.rotate_x(-event.relative.y * 0.01)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+
 
 
 func _physics_process(delta):
@@ -31,7 +41,17 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	if Input.is_action_pressed("crouch"):
+		is_crouching = true
+		current_height = crouch_height
+		print(is_crouching)
+	else:
+		is_crouching = false
+		current_height = original_height
+		print(is_crouching)
+	
+	$CollisionShape3D.scale = current_height
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
